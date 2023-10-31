c0c25f615daff8141a0b13118e12d3c3c9d2d8a3
Karroninja <lindrooskaroline@gmail.com>
2023-10-31 17:32:03 +0800

    edited files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 "use strict";
 
-const getDatabase = new Request("\db/database.json");
+const getDatabase = new Request("\db/database.json", {
+    method: "GET",
+    header: {"Content-Type": "application/json"}
+})
 console.log(getDatabase)
 fetch(getDatabase)
-    .then(res => res.json)
+    .then(res => {
+        if (!res.ok) {
+            console.info(res)
+        }
+        return res.json();
+    })
     .then(data => console.info(data))
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
